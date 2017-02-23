654ad5688a297d097e6b96e07d08cb33d8ea5cc2
Dustin <dward@unidev.com>
2017-02-23 05:19:41 +0000

    initial

---
+++ main.py
@@ -0,0 +1,45 @@
+import webapp2
+import os
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+								autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class Art(db.Model):
+	
+
+class MainPage(Handler):
+	def render_front(self, title="", art="", error=""):
+		self.render("front.html", title=title, art=art, error=error)
+
+	def get(self):
+		self.render_front()
+
+	def post(self):
+		title = self.request.get("title")
+		art = self.request.get("art")
+
+		if title and art:
+			self.write("thanks!")
+		else:
+			error = "we need both a title and some artwork"
+			self.render_front(title, art, error)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
